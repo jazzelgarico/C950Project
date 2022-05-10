24c50c342766c8c061e06187d4a7d7bdf7a5fa99
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-05-10 15:00:49 -0500

    Added constant DISTANCES_FILENAME

--- src/wgups/main.py
+++ src/wgups/main.py
@@ -4,6 +4,7 @@ import fileimport
 
 AVG_PACKAGES_PER_DAY = 40
 PACKAGES_FILENAME = 'WGUPS Package File.csv'
+DISTANCES_FILENAME = 'WGUPS Distance Table.csv'
 
 packages = package.PackageHashTable(AVG_PACKAGES_PER_DAY * 2)
 fileimport.receive_packages(PACKAGES_FILENAME, packages)
