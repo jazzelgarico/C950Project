4585f16b60f881896e6f1bb95130bff3611abdea
Jazz Elgarico <jazz.elgarico@gmail.com>
2022-05-11 13:17:39 -0500

    Revised truck to use item loader, started class Loader

--- src/wgups/truck.py
+++ src/wgups/truck.py
@@ -11,16 +11,43 @@ class Truck:
     hour, leaves the hub no earlier than 8:00AM. Loading and delivering
     are instantaneous.
     """
+
     def __init__(self):
         self.mileage = 0
         self.package_list = []
         self.location = None
 
     def deliver(self, item):
+        """Delivers a package.
+
+        Updates the truck's location, mileage, and the package's
+        delivery status to "Delivered at [time delivered]"
+
+        Args:
+            item: the package to be delivered
+
+        """
         item.update_delivery_status("Delivered")
+        miles_driven = 0  # UPDATE
         self.package_list.remove(item)
+        self.mileage += miles_driven
 
-    def load(self, item):
-        self.package_list.append(item)
-        item.update_delivery_status("En route")
+    def load(self, item_loader):
+        """Loads the truck.
 
+        Updates package_list according to the item_loader
+
+        Args:
+            item_loader: a list of packages to load into the truck
+        """
+        for item in item_loader():
+            self.package_list.append(item)
+            item.update_delivery_status("En route")
+
+
+class Loader:
+    """Determines which packages to load into a Truck.
+
+    """
+    def __init__(self, package_table):
+        self.load_list = []
